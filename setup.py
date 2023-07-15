671dd7645b91ab724ea13592d8e6e43874c2ba5b
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2023-07-15 15:14:32 +0200

    feat: add build phase

--- setup.py
+++ setup.py
@@ -1,49 +1,59 @@
 #!/usr/bin/env python
 
 """The setup script."""
-
+import os
 from setuptools import setup, find_packages
+from setuptools.extern.packaging import version
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
+
+solid_node_version = "0.0.1"
+
+try:
+    solid_node_version = str(version.parse(os.getenv("SOLID_NODE_VERSION")))
+except Exception:
+    pass
 
 setup(
     author="Luis Fagundes",
-    author_email='lhfagundes@gmail.com',
-    python_requires='>=3.6',
+    author_email="lhfagundes@gmail.com",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Affero General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="A framework to develop and manage solid projects in Python",
     entry_points={
-        'console_scripts': [
-            'solid=solid_node.manager:manage',
+        "console_scripts": [
+            "solid=solid_node.manager:manage",
         ],
     },
     install_requires=requirements,
     license="GNU Affero General Public License v3",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='solid_node',
-    name='solid_node',
-    packages=find_packages(include=['solid_node', 'solid_node.*']),
-    test_suite='tests',
+    keywords="solid_node",
+    name="solid_node",
+    packages=find_packages(include=["solid_node", "solid_node.*"]),
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/lfagundes/solid_node',
-    version='0.0.1',
+    url="https://github.com/LibreSolid/solid_node",
+    version=solid_node_version,
     zip_safe=False,
 )
